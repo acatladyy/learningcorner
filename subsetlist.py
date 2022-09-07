b8fca0cdcbcd7a2d3b02fccdd39ecae692231482
acatladyy <112776053+acatladyy@users.noreply.github.com>
2022-09-06 20:45:00 -0400

    Create subsetlist.py

---
+++ subsetlist.py
@@ -0,0 +1,26 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[3])
+
+# Print out last element from areas
+print(areas[9])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[3])
+
+# Print out last element from areas
+print(areas[9])
+
+# Print out the area of the living room
+print(areas[5])
+18.0
+9.5
+20.0
